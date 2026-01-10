8fdcf05df292c060c10a462ef03988399d6da1c6
Elia Renzoni <elia.renzoni03@gmail.com>
2026-01-10 15:11:21 +0100

    refactor: update lexing policies
    Signed-off-by: Elia Renzoni <elia.renzoni03@gmail.com>

--- src/cmd_parser.c
+++ src/cmd_parser.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 struct ParserContext {
     char *action;
@@ -8,34 +9,29 @@ struct ParserContext {
     bool *err;
 };
 
-bool isActionLegit(const char *, const int);
+bool isActionLegit(const char *);
 bool isLoki(const char *);
-bool computeTokens(const char cmd[], const char *, struct ParserContext *);
-bool startFlagBasedTokenReaper(const char cmd[], struct ParserContext *);
-bool startSimpleTokenReaper(char *cmd[], struct ParserContext *);
 
 static char *actions[] = {
     "run", "exec", "create", "build",
     "start", "stop", "rm", "ps", "images",
 };
 
-bool parseCommands(const char cmd[], const int argc) {
+static char *tokens[] = {};
+
+bool parseCommands(char cmd[], const int argc) {
     struct ParserContext *ctx;
-    if (argc < 2) 
-        return false;
+    if (argc < 2) return false;
 
-    for (int i = 0; i < argc; i++) {
-        if (!isLoki(&cmd[0]))
-            return false;
+    if (!isLoki(&cmd[1])) return false;
 
-        if (!isActionLegit(&cmd[1], argc))
-            return false;
+    if (!isActionLegit(&cmd[2])) return false;
 
-        ctx = (struct ParserContext *) malloc(sizeof(struct ParserContext));
-        strcpy(&cmd[1], ctx->action);
-
-        computeTokens(cmd, &cmd[1], ctx);
-
+    ctx = (struct ParserContext *) malloc(sizeof(struct ParserContext));
+    strcpy(&cmd[1], ctx->action);
+    
+    for (int i = 2; i < argc; i++) {
+        strcpy(&cmd[i], ctx->validatedTokens);
     }
 
     return false;
@@ -48,37 +44,11 @@ bool isLoki(const char *cmd) {
 // this function is responsible of checking the
 // presence of the main commands supported by Loki
 // in the command line string given by the user.
-bool isActionLegit(const char *cmd, const int len) {
-    for (int i = 0; i < len; i++) {
+bool isActionLegit(const char *cmd) {
+    for (int i = 0; i < strlen(actions); i++) {
         if (strcmp(cmd, actions[i])) 
             return true;
     }
 
     return false;
-}
-
-
-bool computeTokens(const char cmds[], const char *action, struct ParserContext *ctx) {
-    if (strcmp(action, "run") || strcmp(action, "create")) {
-        return startFlagBasedTokenReaper(cmds, ctx);
-    }
-
-    if (strcmp(action, "start") 
-            || strcmp(action, "stop") 
-            || strcmp(action, "rm")) {
-        return startSimpleTokenReaper(cmds, ctx);
-    }
-    return true;
-}
-
-bool startFlagBasedTokenReaper(const char cmds[], struct ParserContext *ctx) {
-    return false;
-}
-
-bool startSimpleTokenReaper(char *cmds[], struct ParserContext *ctx) {
-    if (cmds[2] == NULL) {
-        return false;
-    }
-    strcpy(cmds[2], ctx->validatedTokens);
-    return true;
 }
