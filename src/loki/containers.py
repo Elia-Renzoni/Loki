e94189a2ee01b2601fc50ee07d4e276e7e01a707
Elia Renzoni <elia.renzoni03@gmail.com>
2026-01-14 13:32:20 +0100

    refactor: upate containers class
    Signed-off-by: Elia Renzoni <elia.renzoni03@gmail.com>

--- src/loki/containers.py
+++ src/loki/containers.py
@@ -3,8 +3,26 @@ from loki import commands as cmds
 class Containers:
     def _init__(self, context):
         self.lookup = cmds.build_commands_lookup()
-        self.expected_commands = self.lookup['build']
+        self.expected_commands = self.lookup['run']
         self.context = context
+        self.container_envs = set()
+        self.container_runs = set()
+        self.container_name = None
+        self.container_ports = None
 
     def compile_container(self):
-        pass
+        for exp_cmd in self.expected_commands:
+            if self.context[exp_cmd] is True:
+                self.fill_container(self.context[exp_cmd], exp_cmd)
+
+
+    def fill_container(self, value, cmd):
+        match cmd:
+            case "-r":
+                pass
+            case "-v":
+                pass
+            case "-w":
+                pass
+            case "-name":
+                pass
