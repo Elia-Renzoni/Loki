c1feebd28d38f7060917a17609a0dab2299da6da
Elia Renzoni <elia.renzoni03@gmail.com>
2026-01-14 12:53:21 +0100

    fix: add basic command
    Signed-off-by: Elia Renzoni <elia.renzoni03@gmail.com>

--- src/loki/command.py
+++ src/loki/command.py
@@ -1,5 +1,9 @@
 
-
-
 class Command:
-    pass
+    def __init__(self, context):
+        self.context = context
+        self.optional_target = None
+
+    def compile_command(self):
+        pass
+        
