e800019c14634cc8377f9b6e955d0c634b2518df
Elia Renzoni <elia.renzoni03@gmail.com>
2026-01-15 17:09:31 +0100

    chore: update test
    Signed-off-by: Elia Renzoni <elia.renzoni03@gmail.com>

--- tests/test_command.py
+++ tests/test_command.py
@@ -6,14 +6,20 @@ from loki import cmd_parser as parser
 class TestCommand(unittest.TestCase):
 
     def test_compile_command(self):
-        argv = ["start", "peppe"]
+        argv = ["start", "container_name"]
 
         context = parser.parse_commands(argv)
         self.assertEqual(context.is_context_none(), False)
 
         command = cmd.ManagementCommand(context)
         self.assertEqual(command.get_command(), "start")
-        self.assertEqual(command.get_optional_target(), "peppe")
+        self.assertEqual(command.get_optional_target(), "container_name")
+
+        argv = ["ps"]
+        context = parser.parse_commands(argv)
+        command = cmd.ManagementCommand(context)
+        self.assertEqual(command.get_command(), "ps")
+        self.assertEqual(command.get_optional_target(), None)
 
 if __name__ == '__main__':
     unittest.main()
