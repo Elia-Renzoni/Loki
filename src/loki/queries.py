6921ee65a77732a4f465100784cbacbc96a791f7
Elia Renzoni <elia.renzoni03@gmail.com>
2026-01-30 11:14:28 +0100

    chore: add simple test
    Signed-off-by: Elia Renzoni <elia.renzoni03@gmail.com>

--- src/loki/queries.py
+++ src/loki/queries.py
@@ -5,41 +5,41 @@ tables definition
 
 IMAGE_TABLE = """
 CREATE TABLE IF NOT EXISTS image (
-    image_id INTEGER PRIMARY KEY
-    name     VARCHAR(255) UNIQUE NOT NULL
+    image_id INTEGER PRIMARY KEY,
+    name     VARCHAR(255) UNIQUE NOT NULL,
     workdir  VARCHAR(255) NOT NULL
 );
 """
 
 SCRIPTS_TABLE = """
 CREATE TABLE IF NOT EXISTS scripts (
-    script       VARCHAR(255) NOT NULL
-    image_id     INTEGER NOT NULL
+    script       VARCHAR(255) NOT NULL,
+    image_id     INTEGER NOT NULL,
     FOREIGN KEY(image_id) REFERENCES image(image_id)
 );
 """
 
 CMDS_TABLE = """
 CREATE TABLE IF NOT EXISTS cmd (
-    cmd      VARCHAR(255) NOT NULL
-    image_id INTEGER NOT NULL
+    cmd      VARCHAR(255) NOT NULL,
+    image_id INTEGER NOT NULL,
     FOREIGN KEY(image_id) REFERENCES image(image_id)
 );
 """
 
 COPY_TABLE = """
 CREATE TABLE IF NOT EXISTS copy (
-    target   VARCHAR(255) NOT NULL
-    image_id INTEGER NOT NULL
+    target   VARCHAR(255) NOT NULL,
+    image_id INTEGER NOT NULL,
     FOREIGN KEY(image_id) REFERENCES image(image_id)
 );
 """
 
 PORTS_TABLE = """
 CREATE TABLE IF NOT EXISTS port (
-    port         INTEGER NOT NULL
-    image_id     INTEGER NOT NULL
-    container_id INTEGER NOT NULL
+    port         INTEGER NOT NULL,
+    image_id     INTEGER NOT NULL,
+    container_id INTEGER NOT NULL,
     FOREIGN KEY(image_id) REFERENCES image(image_id),
     FOREIGN KEY(container_id) REFERENCES container(container_id)
 );
@@ -52,8 +52,8 @@ CREATE TABLE IF NOT EXISTS container (
 
 EVN_TABLE = """
 CREATE TABLE IF NOT EXISTS env (
-    env_map      VARCHAR(255) NOT NULL
-    container_id INTEGER NOT NULL
+    env_map      VARCHAR(255) NOT NULL,
+    container_id INTEGER NOT NULL,
     FOREIGN KEY(container_id) REFERENCES container(container_id)
 );
 """
@@ -76,3 +76,10 @@ WHERE container = ?;
 
 FETCH_IMAGES = """
 """
+
+
+
+'''
+health monitoring
+'''
+PING = "PRAGMA integrity_check;"
